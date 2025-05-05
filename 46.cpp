5c6e4893ba9625c10d8f9b36aae12891c7156e8e
MANSI2006 <mansipurohit18@gmail.com>
2025-05-05 13:48:15 +0530

    Create 46.cpp

---
+++ 46.cpp
@@ -0,0 +1,19 @@
+// Write a program to calculate the sum of digits of a given number using a while loop
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number to calculate the sum of its digits" << endl;
+    cin >> n;
+    int sum = 0;
+    while (n != 0)
+    {
+        int rem = n % 10;
+        sum += rem;
+        n /= 10;
+    }
+    cout << sum << endl;
+    return 0;
+}
